9e5822581d314d0043e9c57ad38c8a36580d8b8d
Golovin Sergey <golovin.210@yandex.ru>
2020-10-08 19:04:07 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let money = 100;
+const income = 50;
+let addExpenses = [10, 20, 30];
+let deposit = true;
+const mission = 'Нести добро';
+const period = money / income;
+
+alert('Press Ok');
+console.log('Моя миссия - ' + mission);

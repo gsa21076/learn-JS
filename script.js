93c335dddfcd96dd4036c062ff11dc9a6551a614
Golovin Sergey <gsa21076@mail.ru>
2020-10-09 12:25:57 +0300

    Второй урок

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
-let money = 100;
-const income = 50;
-let addExpenses = [10, 20, 30];
+let money = 67000;
+const income = 'сдача в аренду';
+let addExpenses = 'Kоммуналка, Музыкальная Школа, Кредит';
 let deposit = true;
-const mission = 'Нести добро';
-const period = money / income;
+const mission = 150000;
+const period = 8;
 
-alert('Press Ok');
-console.log('Моя миссия - ' + mission);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месцев');
+console.log('Цель: заработать ' + mission + ' рублей');
+console.log((addExpenses.toLowerCase()).split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);

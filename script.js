8fe4ca33e9eabe390a9142991e738d58ec07195a
gsa21076 <57519287+gsa21076@users.noreply.github.com>
2020-10-20 16:24:15 +0300

    урок №9 дополнительное задание

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
+'use strict';
 
-let money = 100;
-const income = 50;
-let addExpenses = [10, 20, 30];
-let deposit = true;
-const mission = 'Нести добро';
-const period = money / income;
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let data = document.getElementById('timeBlock');
 
-alert('Press Ok');
-console.log('Моя миссия - ' + mission);
+let data2 = document.getElementById('timeBlockShort');
+let endHours = '';
+
+
+// ф-ия окончания слова час
+let endWords = function (hours) {
+  // окончание слова час и секунда
+  if (hours >= 2 && hours <= 4 || hours >= 22 && hours <= 24) {
+    endHours = 'а';
+  } else {
+    if (hours === 1 || hours === 21) {
+      endHours = '';
+    } else {
+      endHours = 'ов';
+    }
+  }
+};
+
+//  вывод формата №1
+let time = function () {
+  const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+  let date = new Date();
+  let day = date.getDate();
+  let year = date.getFullYear();
+  let month = date.toLocaleString('ru', { month: 'long' });
+  let dayweek = week[date.getDay() - 1];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  month = month.substring(0, month.length - 1) + 'я';
+  endWords(hours, minutes, seconds);
+  return 'Сегодня ' + dayweek + ", " + day + ' ' + month + " " + year + " года, " + hours + " час" + endHours + ' ' + minutes + " минут " + seconds + ' секунд';
+
+}
+setInterval(() => data.innerHTML = time(), 1000);
+
+
+
+//добавление 0 перед числом
+let zero = function (item) {
+  if (item < 10) {
+    item = '0' + item;
+  }
+  return item
+};
+
+
+//вывод № 2
+let time2 = function () {
+
+  let date = new Date();
+  let day = zero(date.getDate());
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let hours = zero(date.getHours());
+  let minutes = zero(date.getMinutes());
+  let seconds = zero(date.getSeconds());
+
+  return day + '.' + month + '.' + year + ' - ' + hours + ':' + minutes + ':' + seconds;
+}
+setInterval(() => data2.innerHTML = time2(), 1000);
+

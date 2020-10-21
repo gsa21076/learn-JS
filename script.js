fc860faf08c9aed3e3962578c016b56aba49fbd1
gsa21076 <57519287+gsa21076@users.noreply.github.com>
2020-10-21 20:32:44 +0300

    Урок № 10

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+'use strict';
 
-let money = 100;
-const income = 50;
-let addExpenses = [10, 20, 30];
-let deposit = true;
-const mission = 'Нести добро';
-const period = money / income;
+const books = document.querySelector('.books'),
+  book = document.querySelectorAll('.book'),
+  chapters = document.querySelectorAll('li'),
+  a = document.querySelectorAll('a');
 
-alert('Press Ok');
-console.log('Моя миссия - ' + mission);
+
+// замена текста в ссылке
+a[4].textContent = 'Книга 3. this и Прототипы Объектов';
+
+document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';// замена картинки заднего фона
+document.querySelector('.adv').remove();// удаление рекламы
+
+
+// порядок книг
+books.prepend(book[1]);// помнял 1 и 2
+book[4].after(book[3]);// поменял 3 и 4
+books.append(book[2]);// поставил 6 в конец
+
+
+// восстановление порядка глав
+// во второй книге 
+chapters[4].before(chapters[6]);
+chapters[4].before(chapters[8]);
+chapters[10].before(chapters[2]);
+// в пятой книге
+chapters[48].before(chapters[55]);
+chapters[52].before(chapters[48]);
+chapters[54].before(chapters[51]);
+//вставка главы в шестой книге
+chapters[26].before('Глава 8: За пределами ES6');
+
+
+

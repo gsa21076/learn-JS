db0f8d63c143199adbb1c0f0fd6fb5f9d934aa8d
gsa21076 <57519287+gsa21076@users.noreply.github.com>
2020-10-30 09:19:40 +0300

    fix script.js исправил

--- script.js
+++ script.js
@@ -9,10 +9,19 @@ class First {
 
 class Second extends First {
   hello() {
-    const first = new First();
-    first.hello();
+    console.log('Привет я метод родителя!');
+    this.hello2();
+  }
+
+  hello2() {
     console.log('А я наследуемый метод!');
   }
+
+  // {
+  //   const first = new First();
+  //   first.hello();
+  //   console.log('А я наследуемый метод!');
+  // }
 }
 
 const second = new Second();

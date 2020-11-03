df4465d6f0acb660b0565506d4818173924d1e69
gsa21076 <57519287+gsa21076@users.noreply.github.com>
2020-11-03 21:54:30 +0300

    fix script.js урок 19

--- script/script.js
+++ script/script.js
@@ -39,6 +39,62 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   countTimer('4 november 2020');
+
+
+  // menu
+  const toggleMenu = () => {
+
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+
+    const openMenu = () => {
+      let menuRectWidth = menu.getBoundingClientRect().width,
+        width = document.documentElement.clientWidth,
+        count = 0;
+      menu.style.left = count + 'px';
+      let goLeft = () => {
+        count += 10;
+        if (width > 768) {
+          if (count < width) {
+            menu.style.left = count + 'px';
+            setTimeout(goLeft, 1);
+          }
+        } else {
+          menu.style.transform = 'translate(0)';
+        }
+      };
+      goLeft();
+    };
+    const closeMenu = () => {
+      menu.style.left = '0px';
+      menu.style.transform = 'translate(-100%)';
+    };
+
+    btnMenu.addEventListener('click', openMenu);
+    closeBtn.addEventListener('click', closeMenu);
+    menuItems.forEach(elem => elem.addEventListener('click', closeMenu));
+  };
+  toggleMenu();
+
+
+  // popup
+  const togglePopup = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popupClose = document.querySelector('.popup-close');
+
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        console.log(popup);
+        popup.style.display = 'block';
+      });
+    });
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+  };
+  togglePopup();
+
 });
-
-
